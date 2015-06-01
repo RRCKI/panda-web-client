02053cae26bec11eea715e3db0fbf186bcd6c648
Ivan Tertychnyy <itertychnyy@gmail.com>
2015-06-01 11:36:18 +0300

    + wsgi

--- webpanda/run.py
+++ webpanda/run.py
@@ -2,5 +2,6 @@
 from __future__ import unicode_literals
 from app import app
 import os
-os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = 'true'
-app.run(debug = True)
+if __name__ == '__main__':
+    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = 'true'
+    app.run(debug = True)
