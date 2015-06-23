e93f6364a37e6bc35cf660b3761b43033577dbd1
Ivan Tertychnyy <itertychnyy@gmail.com>
2015-06-23 15:29:08 +0300

    bugfix

--- webpanda/ddm/LocalSEPlugin.py
+++ webpanda/ddm/LocalSEPlugin.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import subprocess
 from common import client_config
 from common.NrckiLogger import NrckiLogger
 
@@ -10,7 +11,7 @@ class LocalSEPlugin():
         self.datadir = client_config.DATA_PATH
 
     def get(self, src, dest):
-        dest = self.datadir + dest + '/' + self.scope + '/' + self.lfn
+        dest = self.datadir + dest
         src = self.datadir + src
         _logger.debug('LOCAL: Try to copy file from %s to %s' % (src, dest))
         try:
@@ -27,3 +28,14 @@ class LocalSEPlugin():
             _logger.error("%s: File not found" % src)
 
         self.get(src, dest)
+
+    def link(self, lfn, dir):
+        lfn = self.datadir + lfn
+        dir = self.datadir + dir
+        _logger.debug('LOCAL: Try to link file from %s to %s' % (lfn, dir))
+        try:
+            proc = subprocess.Popen(['/bin/bash'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+            out = proc.communicate("mkdir -p %s && ln -s %s %s" % (self.datadir + dir, self.datadir + lfn, self.datadir + dir))
+
+        except:
+            _logger.error('Unable to link:%s to %s' % (lfn, dir))
