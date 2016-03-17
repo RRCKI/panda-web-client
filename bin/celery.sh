351018202291d343ccb7b79568bc0ebe818ff034
tertychnyy <itertychnyy@gmail.com>
2016-03-17 13:51:57 +0300

    fixed celery

--- bin/celery.sh
+++ bin/celery.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 source /srv/test/panda-web-client/venv/bin/activate
 source setup.sh
-celery worker -A webpanda.tasks.celery --loglevel=info --concurrency=2
+celery worker -A webpanda.app.celery --loglevel=info --concurrency=2
