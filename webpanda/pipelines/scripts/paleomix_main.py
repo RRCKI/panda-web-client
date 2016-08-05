811a14f9fe3703afafc1489c22b97ce7d37f9c0f
tertychnyy <itertychnyy@gmail.com>
2016-08-05 14:43:57 +0300

    updated pipeline workflow

--- webpanda/pipelines/scripts/paleomix_main.py
+++ webpanda/pipelines/scripts/paleomix_main.py
@@ -28,6 +28,25 @@ def run():
         if current_task is None:
             return WebpandaError('Illegal task ID')
 
+        if current_task.status == 'failed':
+            #TODO: What to do if failed?
+            pipeline.status = 'failed'
+            current_task.modification_time = datetime.utcnow()
+            pipelines_.save(pipeline)
+            continue
+
+        if current_task.status == 'cancelled':
+            #TODO: What to do if cancelled?
+            #TODO: Who or by whom? If by system - resubmit, if by user -nothing?
+            pipeline.status = 'cancelled'
+            current_task.modification_time = datetime.utcnow()
+            pipelines_.save(pipeline)
+            continue
+
+        if current_task.status == 'finished':
+            # Get next_task
+            current_task = pclient.get_next_task(pipeline)
+
         if current_task.status == 'defined':
             if current_task.task_type.method == 'start':
 
@@ -68,7 +87,7 @@ def run():
                 #TO_DO: Run async regime
                 paleomix.run(current_task, current_task.task_type.method) # we already get task from id. Not need to obtain again, is it?
                 # if we use async run, all params must be serializable (BaseQuery is not)
-        #TODO if current_task.status != 'defined' we can operate some steps from 2 others 'cron_tasks'
+                continue
 
 
 def check_running_tasks():
@@ -121,38 +140,3 @@ def check_running_tasks():
             task.modification_time = datetime.utcnow()
             tasks_.save(task)
     return True
-
-
-def check_next_task2():
-    """
-    Checks finished task and starts next one
-    Will work with numbered next_task
-    :return:
-    """
-
-    # Get all pipelines
-    pipelines = pipelines_.find(status='running')
-    for pipeline in pipelines:
-        current_task = pclient.get_current_task(pipeline)
-
-        if current_task.status == 'finished':
-            # Get next_task
-            next_task = pclient.get_next_task(pipeline)
-
-            # Check if last task
-            if pclient.is_finish_task(next_task):
-                pipeline.status = 'finished'
-                pipelines_.save(pipeline)
-                continue
-
-        elif current_task.status == 'failed':
-            #TODO: What to do if failed?
-            pipeline.status = 'failed'
-            pipelines_.save(pipeline)
-            return True
-        elif current_task.status == 'cancelled':
-            #TODO: What to do if cancelled?
-            #TODO: Who or by whom? If by system - resubmit, if by user -nothing?
-            pipeline.status = 'cancelled'
-            pipelines_.save(pipeline)
-            return True
